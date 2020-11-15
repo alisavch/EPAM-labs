f1fa7ade98c70a040e38a443b94c6c293793ae2e
Alina Savchuk <Alina.Savchuk2001@gmail.com>
2020-11-15 12:13:47 +0300

    update

--- CleanCode/Java/src/test/java/AirportTest.java
+++ CleanCode/Java/src/test/java/AirportTest.java
@@ -27,8 +27,8 @@ public class AirportTest {
             new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
             new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
             new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT),
-            new experimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
-            new experimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
+            new ExperimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
+            new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
     );
     Airport airport = new Airport(planes);
 
@@ -36,7 +36,7 @@ public class AirportTest {
 
     @Test
     public void getTransportMilitaryPlanes() {
-        Assert.assertTrue(airport.getTransportMilitaryPlanes().stream().anyMatch(militaryType->militaryType.getMiliratyType()==MilitaryType.TRANSPORT));
+        Assert.assertTrue(airport.getTransportMilitaryPlanes().stream().anyMatch(militaryType->militaryType.getMilitaryType()==MilitaryType.TRANSPORT));
     }
 
     @Test
@@ -51,7 +51,7 @@ public class AirportTest {
 
     @Test
     public void hasAtLeastOneBomberInMilitaryPlanes() {
-        Assert.assertTrue(bomberMilitaryPlanes.stream().filter(mititaryPlane->militaryPlane.getMilitaryType()==MilitaryType.BOMBER));
+        Assert.assertTrue(airport.getBomberMilitaryPlanes.stream().filter(mititaryPlane->militaryPlane.getMilitaryType()==MilitaryType.BOMBER));
     }
 
     @Test
