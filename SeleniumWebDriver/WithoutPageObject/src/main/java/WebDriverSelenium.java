1fe357e85c4e15348050a1ba3390685f82f46eea
Alina Savchuk <Alina.Savchuk2001@gmail.com>
2020-11-25 09:33:29 +0300

    delete one import

--- SeleniumWebDriver/WithoutPageObject/src/main/java/WebDriverSelenium.java
+++ SeleniumWebDriver/WithoutPageObject/src/main/java/WebDriverSelenium.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class WebDriverSelenium{
 
     public static void initialization(){
