54662abc7f934c90a1afd6e6022b69926b2c0454
Alina Savchuk <Alina.Savchuk2001@gmail.com>
2020-11-26 18:09:45 +0300

    tests

--- SeleniumWebDriver/WithoutPageObject/src/test/java/WebDriverSeleniumTest.java
+++ SeleniumWebDriver/WithoutPageObject/src/test/java/WebDriverSeleniumTest.java
@@ -11,27 +11,13 @@ public class WebDriverSeleniumTest extends WebDriverSelenium{
     @Test
     public void verifyNameTest() {
         Assert.assertEquals("Nike React Infinity Run Flyknit Premium", actualNameWE.getText().trim());
-    }
-    @Test
-    public void verifyTitleTest() {
         Assert.assertEquals("Женская беговая обувь", actualTitleWE.getText().trim());
-    }
-    @Test
-    public void verifyColourTest() {
         Assert.assertEquals("Фиолетовый пепел/Розовый взрыв/Ярко-розовый/Dark Smoke Grey", actualColourWE.getText().trim());
-    }
-    @Test
-    public void verifySizeTest() {
         Assert.assertEquals("9 (RU 39,5)", actualSizeWE.getText().trim());
-    }
-//    @Test
-//    public void verifyPriceTest() {
 //        Assert.assertEquals("12 999,00 ₽", actualPriceWE.getText().trim());
-//    }
-    @Test
-    public void verifyCountTest() {
         Assert.assertEquals("1", actualCountWE.getText().trim());
     }
+
     @AfterTest
     public void tearDown(){
         driver.quit();
