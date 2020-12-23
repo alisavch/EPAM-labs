dd05dabcb89cf83230a6ce92616f3517005e33d4
Alina Savchuk <Alina.Savchuk2001@gmail.com>
2020-12-23 18:06:20 +0300

    edit count of click

--- SeleniumWebDriver/Framework/src/test/java/com/myamazingproject/test/NikeCartTest.java
+++ SeleniumWebDriver/Framework/src/test/java/com/myamazingproject/test/NikeCartTest.java
@@ -19,7 +19,6 @@ public class NikeCartTest extends CommonConditions {
     private static final String SNEAKERS_INPUT = "Nike React";
     public static MainPage mainPage;
     public static SneakersPage sneakersPage;
-    public static SneakersPage sneakersPage2;
     public static CartPage cartPage;
     public static SneakersInfo expectedSneakers;
 
@@ -63,9 +62,9 @@ public class NikeCartTest extends CommonConditions {
         cartPage = sneakersPage.chooseSize(SneakersSize.US9)
                 .addToCart()
                 .shoppingCart();
-        sneakersPage2 = new SneakersPage(driver, "https://www.nike.com/ru/t/%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8-%D0%B4%D0%BB%D1%8F-%D0%B7%D0%B0%D0%B1%D0%B5%D0%B3%D0%BE%D0%B2-air-zoom-alphafly-next-g2tqPZ/CZ1514-800");
-        sneakersPage2.openPage();
-        sneakersPage2.chooseSize(SneakersSize.US9)
+        sneakersPage = new SneakersPage(driver, "https://www.nike.com/ru/t/%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8-%D0%B4%D0%BB%D1%8F-%D0%B7%D0%B0%D0%B1%D0%B5%D0%B3%D0%BE%D0%B2-air-zoom-alphafly-next-g2tqPZ/CZ1514-800");
+        sneakersPage.openPage();
+        sneakersPage.chooseSize(SneakersSize.US9)
                 .addToCart()
                 .shoppingCart();
         assertThat(cartPage.getPrice(),is("33 779,00"));
